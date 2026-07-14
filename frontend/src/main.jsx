fe41d2262ed4e7753021cfa7beca267394887d52
Maviya Shaik <shaikmaviya11@gmail.com>
2026-07-14 18:40:18 +0530

    Add admin dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import AdminDashboard from './AdminDashboard.jsx'
-import './AdminDashboard.css'
+import AdminDashboard from './Admindashboard.jsx'
+import './Admindashboard.css'
 
 const isAdmin = window.location.pathname.startsWith('/admin')
 
